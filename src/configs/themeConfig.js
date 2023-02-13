7f31827dc655869cd34c854f054270603ecba0f8
raihanmd <mraihanf471@gmail.com>
2023-02-13 19:24:35 +0700

    tes

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -7,7 +7,7 @@
 
 const themeConfig = {
   // ** Layout Configs
-  templateName: 'App Name' /* App Name */,
+  templateName: 'WhatSchool' /* App Name */,
   layout: 'vertical' /* vertical | horizontal */,
   mode: 'light' /* light | dark | semi-dark /*! Note: semi-dark value will only work for Vertical Layout */,
   direction: 'ltr' /* ltr | rtl */,
